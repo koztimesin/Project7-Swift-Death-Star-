0a5e95919f4a86fdd477f9eec8f56ceb52fb97af
Karthus Saffron <ksaffron@mi-h5.msk.21-school.ru>
2022-02-17 16:28:10 +0300

    first commit

---
+++ Project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Karthus Saffron on 2/17/22.
+//  Copyright © 2022 Karthus Saffron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
